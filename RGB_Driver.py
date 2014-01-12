948b65bf0957d93487f3184eeb20031eea0afaad
Cameron Little <cameron@camlittle.com>
2014-01-11 21:27:30 -0800

    Controllable via a color wheel from the internet

--- RGB_Driver.py
+++ RGB_Driver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-This software can be used to drive an analog RGB LED strip using a raspberry pi 
+This software can be used to drive an analog RGB LED strip using a raspberry pi
 and adafruit's 16-channel 12-bit PWM/Servo Driver - PCA9685:
 http://www.adafruit.com/products/815
 
@@ -28,7 +28,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import Adafruit_PWM_Servo_Driver
-import time
+import time, re
 from random import randrange
 
 
@@ -43,14 +43,14 @@ class RGB_Driver(object):
             self.pwm = pwm
 
     @staticmethod
-    def setup_pwm(freq=200):
+    def setup_pwm(freq = 200):
         pwm = Adafruit_PWM_Servo_Driver.PWM()
         pwm.setPWMFreq(freq)
         return pwm
 
     @staticmethod
     def convert_eight_to_twelve_bit(eight_bit):
-        """The PWM chip has 10 bit resolution, so we need to 
+        """The PWM chip has 10 bit resolution, so we need to
             convert regular 8 bit rgb codes
         >>> RGB_Driver.convert_eight_to_ten_bit(0)
         0
@@ -59,82 +59,78 @@ class RGB_Driver(object):
         >>> RGB_Driver.convert_eight_to_ten_bit(128)
         2048
         """
-        return eight_bit<<4
-        
-           
-    def set_rgb(self, red_value, green_value, blue_value):
-        """The rgb values must be between 0 and 4095"""
-        #print "R: %d, G: %d, B: %d" % (red_value, green_value, blue_value)
-        self.pwm.setPWM(self.red_pin, 0, red_value)
-        self.pwm.setPWM(self.green_pin, 0, green_value)
-        self.pwm.setPWM(self.blue_pin, 0, blue_value)
+        return eight_bit << 4
 
     @staticmethod
     def sanitize_int(x):
-        if x<0:
+        if x < 0:
             return 0
-        elif x>4095:
+        elif x > 4095:
             return 4095
         else:
             return int(x)
 
-
     @staticmethod
-    def randrange(start, stop, step=1):
+    def randrange(start, stop, step = 1):
         """A slightly modified version of randrange which allows start==stop"""
         if start == stop:
             return start
         else:
-            return randrange(start, stop, step) 
-
-    def fade_rgb(self, from_red, from_green, from_blue, to_red, to_green, to_blue, steps, delay):
-        """Fade from one rgb value to another in steps, waiting for delay ms between each step
-            all rgb values must be 10 bit ints (between 0 and 1023)"""
-        red_step = float(to_red - from_red)/steps
-        green_step = float(to_green - from_green)/steps
-        blue_step = float(to_blue - from_blue)/steps
-        
-        for step in range(0, steps+1):
-            red_value = self.sanitize_int(from_red + red_step*step)
-            green_value = self.sanitize_int(from_green + green_step*step)
-            blue_value = self.sanitize_int(from_blue + blue_step*step)
-            self.set_rgb(red_value, green_value, blue_value)
-            time.sleep(delay)
-
-    @staticmethod
-    def get_next_random_value(current_value, minimum_allowed, maximum_allowed, max_walk):
-        """Generate the next random value, given the current, max, and min values of that channel
-            as well as the maximum change (walk) allowed"""
-        min = current_value - max_walk
-        if min<minimum_allowed:
-            min = minimum_allowed
-        elif min<0:
-            min = 0
-
-        max = current_value + max_walk
-        if max>maximum_allowed:
-            max = maximum_allowed
-        elif max>4095:
-            max=4095
-
-        return RGB_Driver.randrange(min, max)
+            return randrange(start, stop, step)
 
 
-    def random_walk(self, min_red, min_green, min_blue, max_red, max_green, max_blue, random_time, delay, max_walk):
-        begin = time.time()
-        red = self.randrange(min_red, max_red)
-        green = self.randrange(min_green, max_green)
-        blue = self.randrange(min_blue, max_blue)
-        self.set_rgb(red, green, blue)
-        time.sleep(delay)
-        while time.time()-begin<random_time:
-            red = self.get_next_random_value(red, min_red, max_red, max_walk)
-            green = self.get_next_random_value(green, min_green, max_green, max_walk)
-            blue = self.get_next_random_value(blue, min_blue, max_blue, max_walk)
-            self.set_rgb(red, green, blue)
-            time.sleep(delay)
-        
-            
+    def set_rgb(self, rgb):
+        """The rgb values must be between 0 and 4095"""
+        #print "R: %d, G: %d, B: %d" % (red_value, green_value, blue_value)
+        self.pwm.setPWM(self.red_pin, 0, rgb[0])
+        self.pwm.setPWM(self.green_pin, 0, rgb[1])
+        self.pwm.setPWM(self.blue_pin, 0, rgb[2])
+
+    def set_rand(self):
+        self.set_rgb((randrange(0, 4080), randrange(0, 4080), randrange(0, 4080)))
+
+    def hex_to_rgb(self, hex_color):
+        hex_match = re.match("^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$", hex_color)
+        if hex_match:
+            r = self.convert_eight_to_twelve_bit(int(hex_match.group(1), 16))
+            g = self.convert_eight_to_twelve_bit(int(hex_match.group(2), 16))
+            b = self.convert_eight_to_twelve_bit(int(hex_match.group(3), 16))
+            return (r, g, b)
+        elif hex_color == "rand" or hex_color == "random":
+            r = randrange(0, 4080)
+            g = randrange(0, 4080)
+            b = randrange(0, 4080)
+            return (r, g, b)
+        else:
+            print "Invalid hex color supplied: {:s}".format(hex_color)
+            return None
+
+    def set_hex_color(self, color):
+        self.set_rgb(self.hex_to_rgb(color))
+
+    def from_to(self, rgb_s, rgb_e, duration, freq = 120):
+        duration = float(duration)
+        rgb = list(rgb_s)
+        rgb_diff = [
+                (rgb_e[0] - rgb_s[0]),
+                (rgb_e[1] - rgb_s[1]),
+                (rgb_e[2] - rgb_s[2])
+            ]
+        print duration
+        print rgb_diff
+
+        start = time.time()
+        while True:
+            elapsed = float(time.time() - start) * 1000
+            if elapsed >= duration:
+                break
+            rgb[0] = rgb_s[0] + rgb_diff[0] * (elapsed / duration)
+            rgb[1] = rgb_s[1] + rgb_diff[1] * (elapsed / duration)
+            rgb[2] = rgb_s[2] + rgb_diff[2] * (elapsed / duration)
+            #time.sleep(float(freq) / 1000)
+            self.set_rgb(map(int, rgb))
+            print elapsed / duration, rgb
+
 
 if __name__ == '__main__':
     import argparse
@@ -145,34 +141,31 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--blue', nargs=2, type=int, default=[0, 0], help="The beginning and end values for blue in fade mode.  Max and min values for blue in random mode..")
     parser.add_argument('-s', '--steps', type=int, default=100, help="Number of steps in the fade.  Not used with --random")
     parser.add_argument('-d', '--delay', type=float, default=0.005, help="Number of seconds between the steps or random changes, can be a float")
-    parser.add_argument('-o', '--turn-off', action='store_true', help="Turn off when the fade or random event is over.")
-    parser.add_argument('--red-pin', type=int, choices=range(0,16), default=0, help="The red pwm pin")
-    parser.add_argument('--green-pin', type=int, choices=range(0,16), default=1, help="The green pwm pin")
-    parser.add_argument('--blue-pin', type=int, choices=range(0,16), default=2, help="The blue pwm pin")
+    parser.add_argument('-o', '--off', action='store_true', help="Turn off after other actions.")
     parser.add_argument('--repeat', type=int, default=1, help="Repeat the fade this many times.  Unused with --random.")
     parser.add_argument('--reverse', action="store_true", help="Reverse the fade, to return back to the initial state.  Unused with --random.")
     parser.add_argument('--random', action="store_true", help="Move around randomly between the max and min values specified with --red --green and --blue.  Use with --time and --max-random-walk")
     parser.add_argument('--time', type=int, default=10, help="Used with --random.  Move randomly for this many seconds")
-    parser.add_argument('--max-random-walk', type=int, default=10, help="The max that each channel will be allowed to change between steps in random mode.") 
+    parser.add_argument('--max-random-walk', type=int, default=10, help="The max that each channel will be allowed to change between steps in random mode.")
+    parser.add_argument('-c', '--color', type=str, help='Set the strip to this color.')
     args = parser.parse_args()
 
-
-
-
-    driver = RGB_Driver(red_pin = args.red_pin, green_pin = args.green_pin, blue_pin = args.blue_pin)
+    driver = RGB_Driver()
     try:
-        if args.random is False:
+        """
+        if args.color:
+            driver.set_hex_color(args.color);
+        elif args.random is False:
             for repeat in xrange(0, args.repeat):
                 print "Repetition %d" % repeat
-                driver.fade_rgb(args.red[0], args.green[0], args.blue[0], args.red[1], args.green[1], args.blue[1], args.steps, args.delay)
-                if args.reverse:
-                    driver.fade_rgb(args.red[1], args.green[1], args.blue[1], args.red[0], args.green[0], args.blue[0], args.steps, args.delay)
         else:
             # We need to sort the values to make sure randrange works correctly
             args.red.sort()
             args.green.sort()
             args.blue.sort()
             driver.random_walk(args.red[0], args.green[0], args.blue[0], args.red[1], args.green[1], args.blue[1], args.time, args.delay, args.max_random_walk)
+        """
+        driver.from_to(driver.hex_to_rgb("#6fff00"), driver.hex_to_rgb("#ae00ff"), 5000)
     finally:
-        if args.turn_off:
-            driver.set_rgb(0, 0, 0)
+        if args.off:
+            driver.set_rgb((0, 0, 0))
---
+++ server.py
@@ -0,0 +1,38 @@
+from bottle import redirect, request, route, run, template, get, post, static_file
+from RGB_Driver import RGB_Driver
+from pprint import pprint
+
+rgb_driver = RGB_Driver()
+
+@get('/')
+def index():
+    return '''
+        <link rel="stylesheet" href="/static/farbtastic/farbtastic.css"></link>
+        <form action="/color" method="post">
+            Color: <input name="color" id="color" type="text" value="#123456">
+        </form>
+        <div id="colorpicker"></div>
+        <script src="/static/jquery-2.0.3.min.js"></script>
+        <script src="/static/farbtastic/farbtastic.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $('#colorpicker').farbtastic(function(c) {
+                    $('#color').val(c);
+                    $.post("/color", { color: c });
+                });
+            });
+        </script>
+    '''
+
+@post('/color')
+def do_color():
+    color = "#" + request.body.read()[9:]
+    rgb_driver.set_hex_color(color)
+    redirect("/")
+
+@route('/static/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='static')
+
+run(host='0.0.0.0', port=8080)
